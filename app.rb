35ec103a9f930bf702e0e0010889df55a104b6bb
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-05-20 10:02:25 -0700

    commit tree a6c644feb714a74fe6a30a4e7ad0559152e00990

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/newteam' do
+        erb :newteam
+    end
 
-
+    post '/newteam' do 
+    @name = params["name"]
+    @coach = params["coach"]
+    @pguard = params["pg"]
+    @sguard = params["sg"]
+    @sfoward = params["sf"]
+    @pforward = params["pf"]
+    @center = params["c"]
+        erb :team
+    end
 end
